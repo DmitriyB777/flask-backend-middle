27077f9ab14562bd55ccda2a88b45c16909b05d5
Dima <belousdima777@yandex.ru>
2026-03-01 03:04:55 +0300

    added controller of city

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from .controllers.region_controller import region_controller
+from .controllers.city_controller import city_controller
 
 def create_app():
     # int app
@@ -7,5 +8,6 @@ def create_app():
 
     # add controllers
     app.register_blueprint(region_controller, url_prefix='/api')
+    app.register_blueprint(city_controller, url_prefix='/api')
 
     return app
---
+++ app/controllers/city_controller.py
@@ -0,0 +1,19 @@
+from flask import Blueprint, jsonify
+
+city_controller = Blueprint('city', __name__)
+
+@city_controller.get('/cities')
+def get_regions():
+    return jsonify({'name': 'hello get'})
+
+@city_controller.post('/city')
+def add_region():
+    return jsonify({'name': 'hello post'})
+
+@city_controller.put('/city')
+def update_region():
+    return jsonify({'name': 'hello put'})
+
+@city_controller.delete('/city')
+def delete_region():
+    return jsonify({'name': 'hello delete'})
